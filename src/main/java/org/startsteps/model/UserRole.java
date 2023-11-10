1b4dc8a917042116f4bb5a95a01cd30118daf400
marishkazachariah <marishka.zachariah@gmail.com>
2023-11-10 09:48:40 +0100

    create UserRole enum for user roles

---
+++ src/main/java/org/startsteps/model/UserRole.java
@@ -0,0 +1,18 @@
+package org.startsteps.model;
+
+public enum UserRole {
+    ADMIN("Admin"),
+    TEACHER("Teacher"),
+    STUDENT("Student");
+
+    private final String roleName;
+
+    UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
+
